015807e31c4d287d1f8bd9e0b1fb4ccb944de8ed
Bread God <Vipcube@users.noreply.github.com>
2022-03-19 00:52:49 +0800

    Finished order in payment process service.

---
+++ Payment-Service/src/main/java/org/vipcube/spring/payment/service/PaymentProcessService.java
@@ -0,0 +1,27 @@
+package org.vipcube.spring.payment.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import org.vipcube.spring.domain.dto.Order;
+import org.vipcube.spring.domain.dto.OrderStatus;
+
+@Slf4j
+@Service
+public class PaymentProcessService {
+	private final IOrderPaymentService orderPaymentService;
+
+	public PaymentProcessService( IOrderPaymentService orderPaymentService ){
+		this.orderPaymentService = orderPaymentService;
+	}
+
+	@KafkaListener( id = "orders", topics = "orders", groupId = "payment")
+	public void onEvent( Order order ) {
+		log.info( "PaymentProcessService: Received order: {}" , order );
+		if ( OrderStatus.NEW == order.getStatus() ) {
+			this.orderPaymentService.reserve( order );
+		} else {
+			this.orderPaymentService.confirm( order );
+		}
+	}
+}
