0fa5727bdfc44701c8f0b17ca5cfdc9dbdef1fd6
Bread God <Vipcube@users.noreply.github.com>
2022-03-18 17:40:05 +0800

    Add the order kafka topic and stream setting.

---
+++ Order-Service/src/main/java/org/vipcube/spring/order/config/KafkaStreamConfig.java
@@ -0,0 +1,38 @@
+package org.vipcube.spring.order.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.JoinWindows;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.StreamJoined;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.support.serializer.JsonSerde;
+import org.vipcube.spring.domain.dto.Order;
+import org.vipcube.spring.order.service.IOrderService;
+
+import java.time.Duration;
+
+@Slf4j
+@Configuration
+public class KafkaStreamConfig {
+	private final IOrderService orderManageService;
+
+	public KafkaStreamConfig( IOrderService orderManageService ) {
+		this.orderManageService = orderManageService;
+	}
+
+	@Bean
+	public KStream<Long, Order> stream( StreamsBuilder builder ) {
+		JsonSerde<Order> orderSerde = new JsonSerde<>( Order.class );
+		KStream<Long, Order> stream = builder.stream( "payment-orders", Consumed.with( Serdes.Long(), orderSerde ) );
+
+		stream.join( builder.stream( "inventory-orders" ), this.orderManageService::confirm,
+						JoinWindows.of( Duration.ofSeconds( 10 ) ), StreamJoined.with( Serdes.Long(), orderSerde, orderSerde ) )
+				.peek( ( k, o ) -> log.info( "OrderService: Current stream order: {}", o ) )
+				.to( "orders" );
+		return stream;
+	}
+}
