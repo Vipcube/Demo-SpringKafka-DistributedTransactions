4ce60f7715d03e15fad43ad45d1465e74acbe699
Bread God <Vipcube@users.noreply.github.com>
2022-03-24 23:53:48 +0800

    DTO supports validation.

--- Domain-DTO/src/main/java/org/vipcube/spring/domain/dto/Order.java
+++ Domain-DTO/src/main/java/org/vipcube/spring/domain/dto/Order.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
 import java.math.BigDecimal;
 
 @Data
@@ -12,12 +14,17 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Order {
+	@Null( message = "id should be empty" )
 	private Long id;
+	@NotNull( message = "customer id should not empty" )
 	private Long customerId;
+	@NotNull( message = "product id should not empty" )
 	private Long productId;
 	private int productCount;
+	@NotNull( message = "price should not empty" )
 	private BigDecimal price;
 	@Builder.Default
 	private OrderStatus status = OrderStatus.NEW;
+	@Null
 	private ServiceSource source;
 }
